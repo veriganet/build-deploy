26b757b1e824c7b70aff248e2b1d1cf1b8fd9905
Fehat Ozkasgari <ferhat@dice.fm>
2022-02-24 00:38:40 -0500

    ReadWriteOnce

--- helm-charts/create_secret.sh
+++ helm-charts/create_secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl -n tst2 delete secret mosquitto-passwd-live
-kubectl -n tst2 create secret generic mosquitto-passwd-live --from-file=passwd
+kubectl -n tst3 delete secret mosquitto-passwd-live
+kubectl -n tst3 create secret generic mosquitto-passwd-live --from-file=passwd
