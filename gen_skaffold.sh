7b017bbbd140d91b1a6b4b85532e182f792f257f
Fehat Ozkasgari <ferhat@dice.fm>
2022-03-11 17:13:27 -0500

    deploy-live 1

--- gen_skaffold.sh
+++ gen_skaffold.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-export SKAFFOLD_RELEASE_NAME=test
-export SKAFFOLD_RELEASE_NAMESPACE=test
-export LIVE_NODE_PEERING_PORT=7176
-export ABBREVIATION=test
-export DOMAINSVC=verigasvc.com
 
-sed 's/_SKAFFOLD_RELEASE_NAME_/'"$SKAFFOLD_RELEASE_NAME"'/g' helm-charts/skaffold.yml > helm-charts/skaffold.yaml
-sed -i 's/_SKAFFOLD_RELEASE_NAMESPACE_/'"$SKAFFOLD_RELEASE_NAMESPACE"'/g' helm-charts/skaffold.yaml
-sed -i 's/_LIVE_NODE_PEERING_PORT_/'"$LIVE_NODE_PEERING_PORT"'/g' helm-charts/skaffold.yaml
-sed -i 's/_ABBREVIATION_/'"$ABBREVIATION"'/g' helm-charts/skaffold.yaml
-sed -i 's/_DOMAINSVC_/'"$DOMAINSVC"'/g' helm-charts/skaffold.yaml
+sed -i 's/_ABBREVIATION_/'"$ABBREVIATION"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_DOMAINSVC_/'"$DOMAINSVC"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_LIVE_NODE_PEERING_PORT_/'"$LIVE_NODE_PEERING_PORT"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_KUBERNETES_STORAGE_CLASS_TYPE_/'"$KUBERNETES_STORAGE_CLASS_TYPE"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_KUBERNETES_STORAGE_CLASS_NAME_/'"$KUBERNETES_STORAGE_CLASS_NAME"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_BPOW_KEY_/'"$BPOW_KEY"'/g' helm-charts/skaffold.yml
+
+sed -i 's/_MOSQUITTO_PASSWD_/'"$MOSQUITTO_PASSWD"'/g' helm-charts/skaffold.yml
+
+
